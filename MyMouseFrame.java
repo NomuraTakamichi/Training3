31a090bf82dbfbfa06622330e8dff5419525b0b7
otaoaki <taiaki1102@gmail.com>
2019-11-04 13:19:44 +0900

    MyApp 코드를 작성

--- MyMouseFrame.java
+++ MyMouseFrame.java
@@ -1,11 +1,13 @@
-
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
 /**
  * 여기에 MyMouseFrame 클래스 설명을 작성하십시오.
  * 
  * @author (작성자 이름) 
  * @version (버전번호나 날짜)
  */
-public class MyMouseFrame
+public class MyMouseFrame extends JFrame
 {
     // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 바꾸십시오
     private int x;
